deb6e11aa296680863cb9057e854c053e335f10d
Chungsang Tom Lam <erinata@gmail.com>
2025-10-03 09:42:34 -0400

    tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 // vite.config.js
 import { defineConfig } from 'vite'
-
-// If you're using React/Vue/Svelte, also import and add the framework plugin.
-// Example for React: import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   base: '/impermanent_loss/',                 // << include trailing slash
   build: { outDir: 'docs' },        // output to /docs for GitHub Pages
-  // plugins: [react()],             // (uncomment if you use React)
+  plugins: [
+    tailwindcss(),
+    ],
 })
+  
